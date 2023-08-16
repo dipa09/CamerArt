64b8b905b62bdfef26478856c83eb0f98e5b0267
Luca <lucaiseppi213@gmail.com>
2023-08-16 17:50:32 +0200

    Added required permissions

--- app/src/main/java/com/example/camerart/MainActivity.kt
+++ app/src/main/java/com/example/camerart/MainActivity.kt
@@ -56,10 +56,10 @@ class MainActivity : AppCompatActivity() {
             mutableListOf(
                 Manifest.permission.CAMERA,
                 Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.READ_EXTERNAL_STORAGE
             ).apply {
                 if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
                     add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    add(Manifest.permission.READ_EXTERNAL_STORAGE)
                 }
             }.toTypedArray()
 
